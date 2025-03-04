14d8ba8bf20221058bf54b3ec570eca860235286
Ahnnyong <itiscool7@naver.com>
2025-03-04 17:19:58 +0900

    초기 커밋 메시지

---
+++ src/main/java/com/springboot/oauthjwt/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.springboot.oauthjwt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String manAPI() {
+        return "main router";
+    }
+}
